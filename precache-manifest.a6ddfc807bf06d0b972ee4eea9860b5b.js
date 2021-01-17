bcfbbe996721abf94f4a909fe26a2e0c84d710f8
Sruthi <sruthivellore99@gmail.com>
2021-01-17 21:49:47 +0530

    Updates

---
+++ precache-manifest.a6ddfc807bf06d0b972ee4eea9860b5b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ec2911f32da4de4e46406ff01afccd",
+    "url": "/Hangman/index.html"
+  },
+  {
+    "revision": "dd871d248ea68a9c7d2a",
+    "url": "/Hangman/static/css/2.1fb2c54e.chunk.css"
+  },
+  {
+    "revision": "ccf561557ff4c56c78a5",
+    "url": "/Hangman/static/css/main.ee213594.chunk.css"
+  },
+  {
+    "revision": "dd871d248ea68a9c7d2a",
+    "url": "/Hangman/static/js/2.58067d4e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Hangman/static/js/2.58067d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf561557ff4c56c78a5",
+    "url": "/Hangman/static/js/main.a3ce1d81.chunk.js"
+  },
+  {
+    "revision": "69478adb882d8b63b804",
+    "url": "/Hangman/static/js/runtime-main.04637f3a.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/Hangman/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/Hangman/static/media/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/Hangman/static/media/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/Hangman/static/media/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/Hangman/static/media/primeicons.b0f5d02f.eot"
+  }
+]);
